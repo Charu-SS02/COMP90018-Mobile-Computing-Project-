f2a7c057dc816ea2fbd3757f3ce3d6353bf9f55b
RobGadsby <rgadsby@student.unimelb.edu.au>
2020-11-02 10:29:53 +0000

    starting to use constructors in api getter class

--- app/src/main/java/com/example/findcoffee/ui/search/SearchFragment.java
+++ app/src/main/java/com/example/findcoffee/ui/search/SearchFragment.java
@@ -81,13 +81,13 @@ public class SearchFragment extends Fragment {
                     map.put("category=", "6");
 
                     map.put("q=", searchInputString);
-                    map.put("radius=", "5000");
+                    map.put("radius=", "500");
 
 
 //        apiGetter.search(map, this, 10);
 
 
-                    zomato.search(map,queue,9999);
+                    zomato.search(map,queue,10);
                 }
 
             }
@@ -112,6 +112,10 @@ public class SearchFragment extends Fragment {
                 address,
                 thumb
         ));
+    }
+
+    public void emptyData(){
+        data.clear();
     }
 
     public static RecyclerView getRecyclerView() {
