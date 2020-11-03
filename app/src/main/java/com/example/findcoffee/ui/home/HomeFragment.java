24372ac048f3be99cca77f7cc773e5b9f058458b
Charu-SS02 <charusmitas@student.unimelb.edu.au>
2020-11-03 15:54:23 +1100

    Get current location and display on map

--- app/src/main/java/com/example/findcoffee/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/findcoffee/ui/home/HomeFragment.java
@@ -1,9 +1,13 @@
 package com.example.findcoffee.ui.home;
 
+import android.annotation.SuppressLint;
 import android.location.Address;
 import android.location.Geocoder;
+import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Looper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,11 +20,18 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.findcoffee.R;
 import com.example.findcoffee.zomatoApiGetter;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.io.IOException;
@@ -40,6 +51,13 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
     ArrayList<String> cafeNames = new ArrayList<>();
     ArrayList<String> cafeAddresses = new ArrayList<>();
     ArrayList<LatLng> cafeCoordinates = new ArrayList<>();
+
+    GoogleMap mGoogleMap;
+    SupportMapFragment mapFrag;
+    LocationRequest mLocationRequest;
+    Location mLastLocation;
+    Marker mCurrLocationMarker;
+    FusedLocationProviderClient mFusedLocationClient;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     public View onCreateView(@NonNull LayoutInflater inflater,
@@ -74,6 +92,11 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
 
         Geocoder coder = new Geocoder(root.getContext());
 
+//        for (int i = 0; i < data.size(); i++) {
+//            cafeNames.add(data.get(i).getName());
+//            cafeAddresses.add(data.get(i).getVersion());
+//        }
+
         cafeNames.add("Humble Rays");
         cafeAddresses.add("71 Bouverie Street, Carlton, Melbourne");
 
@@ -90,24 +113,75 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback {
             }
         }
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
-        mapFragment.getMapAsync(this);
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(root.getContext());
+
+        mapFrag = (SupportMapFragment) getChildFragmentManager().findFragmentById(R.id.map);
+        assert mapFrag != null;
+        mapFrag.getMapAsync(this);
 
         return root;
     }
 
+    @SuppressLint("MissingPermission")
     @Override
     public void onMapReady(GoogleMap googleMap) {
+        mGoogleMap = googleMap;
+
+        mLocationRequest = new LocationRequest();
+        mLocationRequest.setInterval(120000); // two minute interval
+        mLocationRequest.setFastestInterval(120000);
+        mLocationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
+
+        mFusedLocationClient.requestLocationUpdates(mLocationRequest, mLocationCallback, Looper.myLooper());
+        mGoogleMap.setMyLocationEnabled(true);
+
         for (int i = 0; i < cafeCoordinates.size(); i++) {
-            googleMap.addMarker(new MarkerOptions()
+            mGoogleMap.addMarker(new MarkerOptions()
                     .position(cafeCoordinates.get(i))
                     .title(cafeNames.get(i))
             );
         }
 
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(cafeCoordinates.get(0)));
-        googleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
+        mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(cafeCoordinates.get(0)));
+        mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(12));
     }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        //stop location updates when Activity is no longer active
+        if (mFusedLocationClient != null) {
+            mFusedLocationClient.removeLocationUpdates(mLocationCallback);
+        }
+    }
+
+    LocationCallback mLocationCallback = new LocationCallback() {
+        @Override
+        public void onLocationResult(LocationResult locationResult) {
+            List<Location> locationList = locationResult.getLocations();
+            if (locationList.size() > 0) {
+                //The last location in the list is the newest
+                Location location = locationList.get(locationList.size() - 1);
+                Log.i("MapsActivity", "Location: " + location.getLatitude() + " " + location.getLongitude());
+                mLastLocation = location;
+                if (mCurrLocationMarker != null) {
+                    mCurrLocationMarker.remove();
+                }
+
+                //Place current location marker
+                LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+                MarkerOptions markerOptions = new MarkerOptions();
+                markerOptions.position(latLng);
+                markerOptions.title("Your current location");
+                markerOptions.icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+                mCurrLocationMarker = mGoogleMap.addMarker(markerOptions);
+
+                //move map camera
+                mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 12));
+            }
+        }
+    };
 
     public void drawShop(String name, String address, String thumb){
         data.add(new HomeViewModel(
